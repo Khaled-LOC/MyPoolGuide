fc588e2356d9dd5dc976dfe58d4de6202ff1298f
Khaled-LOC <khaled@locsoftware.com>
2021-05-31 09:59:28 -0400

    add tree view
    add tree view to the list of titles to make things more clear

---
+++ Testing_js/_vti_cnf/script.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|KAHLED-LAPTOP\\Khaled
+vti_modifiedby:SR|KAHLED-LAPTOP\\Khaled
+vti_timelastmodified:TR|26 May 2021 13:10:46 -0000
+vti_timecreated:TR|26 May 2021 13:10:40 -0000
+vti_title:SR|Untitled 1
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|GitHub/MyPoolGuide/Testing_index.html
+vti_cacheddtm:TX|26 May 2021 13:10:46 -0000
+vti_filesize:IR|335
+vti_nexttolasttimemodified:TR|26 May 2021 13:10:40 -0000
